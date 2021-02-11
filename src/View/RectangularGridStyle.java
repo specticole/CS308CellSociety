b8adee2f621342748c77bd62cde9084a892cfe83
Bill Guo <wg78@duke.edu>
2021-02-11 11:30:01 -0500

    created RectangularGridStyle template

--- src/View/RectangularGridStyle.java
+++ src/View/RectangularGridStyle.java
@@ -1,5 +1,27 @@
 package View;
 
-public class RectangularGridStyle {
+import cellsociety.CellGrid;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
+public class RectangularGridStyle extends GridStyle{
+
+  Pane pane;
+  Rectangle[][] grid;
+
+  public Rectangle createRectangleCell(double width, double height, double xPos, double yPos, Color color){
+    Rectangle cell = new Rectangle(width, height);
+
+  }
+
+  @Override
+  public void createGrid(int width, int height) {
+
+  }
+
+  @Override
+  public void updateGrid(CellGrid cellGrid, int time) {
+
+  }
 }
