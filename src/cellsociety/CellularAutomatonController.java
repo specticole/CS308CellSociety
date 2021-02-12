4bbdd2387f17fa2430789548b112dc7efe60e3c2
Patrick Liu <pyl8@duke.edu>
2021-02-11 22:38:05 -0500

    Added Javadoc comments

--- src/cellsociety/CellularAutomatonController.java
+++ src/cellsociety/CellularAutomatonController.java
@@ -1,28 +1,14 @@
 package cellsociety;
 
-import java.io.IOException;
-import java.util.*;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-import java.io.File;
-import org.xml.sax.SAXException;
-
+/**
+ * This class initializes a CellularAutomatonConfiguration object that stores
+ * all relevant properties of the simulation, and then initializes an appropriate
+ * Model and View object. This class also runs steps of the simulation.
+ *
+ * @author Patrick Liu
+ */
 public class CellularAutomatonController {
   private String configFileName;
-
-  // Outputs of loadConfigFile method
-  private HashMap<String, String> metaMap; // keys: title, author, description
-  private String gridType;
-  private int gridWidth;
-  private int gridHeight;
-  private List<List<String>> gridInitialStates; // inner lists are rows of the grid
-  private HashMap<String, ArrayList<Integer>> cellStyleMap; // keys are String representations of cell states, values are ArrayLists of RGB values
-  private HashMap<String, String> parameterMap; // keys are parameter names, values are parameter values
 
   public CellularAutomatonController(String configFileName) {
     this.configFileName = configFileName;
