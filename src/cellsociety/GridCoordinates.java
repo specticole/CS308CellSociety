165bb74e86e84b878c4570227d210c213d3064f3
Franklin Wei <franklin@rockbox.org>
2021-02-12 23:56:38 -0500

    Work on partially implementing grid-related classes.

---
+++ src/cellsociety/GridCoordinates.java
@@ -0,0 +1,7 @@
+package cellsociety;
+
+/**
+ *
+ */
+public abstract class GridCoordinates {
+}
---
+++ src/cellsociety/grids/RectangularCellGrid.java
@@ -0,0 +1,14 @@
+package cellsociety.grids;
+
+/**
+ * Lists-of-lists, non-wrapping implementation of a rectangular
+ * CellGrid.
+ *
+ * @author Franklin Wei
+ */
+public class RectangularCellGrid extends CellGrid {
+  int width, height;
+
+  public RectangularCellGrid(int w, int h) {
+  }
+}
