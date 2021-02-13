a718b351f91c2661c239665c1d7645ef9aba5810
Bill Guo <wg78@duke.edu>
2021-02-13 15:16:06 -0500

    renamed button labels

--- src/cellsociety/view/CellularAutomatonView.java
+++ src/cellsociety/view/CellularAutomatonView.java
@@ -1,8 +1,10 @@
 package cellsociety.view;
 
+import cellsociety.CellularAutomatonController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 
 
 public class CellularAutomatonView {
@@ -11,10 +13,25 @@ public class CellularAutomatonView {
   private GridPane masterLayout;
   @FXML
   private GridPane mainGrid;
+  @FXML
+  private Button startResetButton;
 
+
+  CellularAutomatonController controller;
 
   public void initialize(){
+    controller = new CellularAutomatonController("initial/xml");
     RectangularGridStyle grid = new RectangularGridStyle(mainGrid);
     grid.createGrid(100,100);
+  }
+
+
+  public void startButtonClick() {
+    controller.playSimulation();
+
+  }
+
+  public void pauseButtonClick() {
+    controller.pauseSimulation();
   }
 }
