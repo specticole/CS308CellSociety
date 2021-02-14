74562c2288c31b0b2e3914c06b9740704ceeb8b4
Franklin Wei <franklin@rockbox.org>
2021-02-14 11:13:24 -0500

    Expand GameOfLifeTest.

--- src/cellsociety/test/GameOfLifeTest.java
+++ src/cellsociety/test/GameOfLifeTest.java
@@ -3,17 +3,46 @@ package cellsociety.test;
 import cellsociety.*;
 import cellsociety.grids.*;
 import cellsociety.rules.*;
+import cellsociety.states.*;
 
 public class GameOfLifeTest {
+  private static void dumpState(CellState state[][]) {
+    System.out.printf("State is %dx%d\n", state[0].length, state.length);
+    for(int y = 0; y < state.length; y++) {
+      for(int x = 0; x < state[y].length; x++) {
+        String s = state[y][x].toString();
+
+        System.out.printf("%s", s.charAt(0) == 'D' ? ' ' : s.charAt(0));
+      }
+      System.out.println("");
+    }
+  }
+
   public static void main(String args[]) {
-    CellGrid grid = new RectangularCellGrid(20, 10,
-                                            false, 8);
+    int w = 20, h = 10;
+    RectangularCellGrid grid = new RectangularCellGrid(w, h,
+                                                       false, 8);
+
+    CellState initialState[][] = new GameOfLifeState[h][w];
+    for(int y = 0; y < h; y++) {
+      for(int x = 0; x < w; x++) {
+        initialState[y][x] = new GameOfLifeState(Math.random() > .9 ?
+                                                 GameOfLifeState.States.ALIVE :
+                                                 GameOfLifeState.States.DEAD);
+      }
+    }
+
+    grid.appendStates(initialState);
 
     CellularAutomatonRule rule = new GameOfLifeRule(null);
 
     CellularAutomaton ca = new CellularAutomaton(grid, rule);
 
     for(;;) {
+      System.out.printf("state at time=%d:\n", ca.getGrid().getCurrentTime());
+
+      dumpState(((Dense2DCellGrid)ca.getGrid()).extractStates(0));
+
       System.out.println("step");
       ca.step();
     }
