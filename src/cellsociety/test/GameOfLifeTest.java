9502afed0d9240bb16d406b464eb3ee57f9f06eb
Franklin Wei <franklin@rockbox.org>
2021-02-13 21:34:40 -0500

    First inklings of a command-line driver program.

---
+++ src/cellsociety/test/GameOfLifeTest.java
@@ -0,0 +1,13 @@
+package cellsociety.test;
+
+import cellsociety.*;
+import cellsociety.grids.*;
+
+public class GameOfLifeTest {
+  public static void main(String args[]) {
+    RectangularCellGrid grid = new RectangularCellGrid(20, 10,
+                                                       false, 8);
+
+
+  }
+}
