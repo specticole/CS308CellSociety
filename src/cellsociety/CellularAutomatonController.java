77060aa15abf0399628107ebb69e1ae83aff68d4
Patrick Liu <pyl8@duke.edu>
2021-02-13 21:43:16 -0500

    Removed unnecessary imports

--- src/cellsociety/CellularAutomatonController.java
+++ src/cellsociety/CellularAutomatonController.java
@@ -1,8 +1,6 @@
 package cellsociety;
 
 import cellsociety.view.CellularAutomatonView;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
