def5304c6ee329af73d2f22b1ad2dabcc6c14db1
Franklin Wei <franklin@rockbox.org>
2021-02-13 21:42:55 -0500

    Flesh out test program.

--- src/cellsociety/test/GameOfLifeTest.java
+++ src/cellsociety/test/GameOfLifeTest.java
@@ -2,12 +2,20 @@ package cellsociety.test;
 
 import cellsociety.*;
 import cellsociety.grids.*;
+import cellsociety.rules.*;
 
 public class GameOfLifeTest {
   public static void main(String args[]) {
-    RectangularCellGrid grid = new RectangularCellGrid(20, 10,
-                                                       false, 8);
+    CellGrid grid = new RectangularCellGrid(20, 10,
+                                            false, 8);
 
+    CellularAutomatonRule rule = new GameOfLifeRule(null);
 
+    CellularAutomaton ca = new CellularAutomaton(grid, rule);
+
+    for(;;) {
+      System.out.println("step");
+      ca.step();
+    }
   }
 }
