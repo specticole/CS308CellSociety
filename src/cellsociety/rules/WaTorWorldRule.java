94a63d988d787789531bef58119a03c927c6ebe4
Cole Spector <cgs26@duke.edu>
2021-02-14 15:10:34 -0500

    added shark and fish moving/eating behavior

--- src/cellsociety/rules/WaTorWorldRule.java
+++ src/cellsociety/rules/WaTorWorldRule.java
@@ -4,7 +4,9 @@ import cellsociety.Cell;
 import cellsociety.CellState;
 import cellsociety.CellularAutomatonRule;
 import cellsociety.states.WaTorWorldState;
+import cellsociety.states.WaTorWorldState.States;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -13,6 +15,9 @@ import java.util.Random;
 public class WaTorWorldRule extends CellularAutomatonRule {
 
   private int[] usedNeighbors = {1,3,4,6};
+  private int sharkRoundsToBreed;
+  private int fishRoundsToBreed;
+
 
   public WaTorWorldRule(Map<String, String> params) {
     super(params);
@@ -20,39 +25,62 @@ public class WaTorWorldRule extends CellularAutomatonRule {
 
   @Override
   public void advanceCellState(Cell cell, List<Cell> neighbors) {
-//    CellState cellState = cell.getState();
-//    if(cellState.getStateAsString().equals("FISH")){
-//      Cell swapCell = swapLogic(cell, neighbors);
-//      if (swapCell != null){
-//        cell.swapCells(swapCell);
-//      }
-//    } else if(cellState.getStateAsString().equals("SHARK")){
-//      Cell swapCell = swapLogic(cell, neighbors);
-//      if (swapCell != null){
-//        cell.swapCells(swapCell);
-//      }
-//    }
+    if(cell.getState(Cell.CURRENT_TIME).getState() == WaTorWorldState.States.FISH){
+      swapLogic(cell, neighbors);
+    }
+
+    if(cell.getState(Cell.CURRENT_TIME).getState() == WaTorWorldState.States.SHARK){
+      if(!findFish(neighbors)){
+        swapLogic(cell, neighbors);
+      }
+
+    }
   }
 
-  private Cell swapLogic(Cell currentCell, List<Cell> neighbors){
-//    int currentTime = currentCell.getParentGrid().getCurrentTime();
-//    ArrayList<Cell> possibleSwaps = new ArrayList<>();
-//    for(int neighborIndex : usedNeighbors){
-//
-//      Cell neighbor = neighbors.get(neighborIndex);
-//
-//      if(neighbor.getState().getStateAsString().equals("OPEN")){
-//        if(neighbor.getPriorStates().getLatestTime() == currentTime){
-//          possibleSwaps.add(neighbor);
-//        }
-//      }
-//    }
-//    if(possibleSwaps.size() == 0){
-//      return null;
-//    } else {
-//      Random rand = new Random();
-//      return possibleSwaps.get(rand.nextInt(possibleSwaps.size()));
-//    }
-    return null;
+  private Boolean findFish(List<Cell> neighbors){
+    ArrayList<Cell> possibleFood = new ArrayList<>();
+    for (int neighborIndex : usedNeighbors) {
+      Cell neighbor = neighbors.get(neighborIndex);
+      if(neighbor.getState(Cell.CURRENT_TIME).getState() == WaTorWorldState.States.FISH){
+        possibleFood.add(neighbor);
+      }
+    }
+
+    if(possibleFood.size() > 0){
+      Random rand = new Random();
+      possibleFood.get(rand.nextInt(possibleFood.size())).appendState(new WaTorWorldState());
+      return true;
+    }
+    return false;
+  }
+
+
+  private void swapLogic(Cell currentCell, List<Cell> neighbors) {
+    ArrayList<Cell> possibleSwaps = new ArrayList<>();
+    for (int neighborIndex : usedNeighbors) {
+      Cell neighbor = neighbors.get(neighborIndex);
+      if(neighbor.getState(Cell.CURRENT_TIME).getState() == WaTorWorldState.States.EMPTY){
+        int currentTime = currentCell.getParentGrid().getCurrentTime();
+        if(neighbor.getStates().getLatestTime() == currentTime){
+          possibleSwaps.add(neighbor);
+        }
+      }
+    }
+    if(possibleSwaps.size() > 0){
+      Random rand = new Random();
+      currentCell.swapCells(possibleSwaps.get(rand.nextInt(possibleSwaps.size())));
+    }
+  }
+
+  /**
+   * This method gets the specific rule set for the game of live variation, in the form of F<int>.../S<int>...
+   * where F is the number of rounds a fish needs to survive in order to breed
+   * where S is the number of rounds a shark needs to survive in order to breed
+   */
+  public void setGameSpecifics() {
+    //this will change to read in from the xml
+
+    sharkRoundsToBreed = 3;
+    fishRoundsToBreed = 3;
   }
 }
