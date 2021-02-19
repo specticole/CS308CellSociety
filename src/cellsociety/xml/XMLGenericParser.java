8606132f048e1e062e50cfeadcd279e2599871c3
Patrick Liu <pyl8@duke.edu>
2021-02-18 22:14:12 -0500

    Added XMLException handling in XMLGenericParser for missing arguments

--- src/cellsociety/xml/XMLGenericParser.java
+++ src/cellsociety/xml/XMLGenericParser.java
@@ -42,6 +42,16 @@ public class XMLGenericParser {
     }
   }
 
+  protected Element getElement(Element parent, String elementName) throws XMLException {
+    NodeList nodeList = parent.getElementsByTagName(elementName);
+    if (nodeList != null && nodeList.getLength() > 0) {
+      return (Element) parent.getElementsByTagName(elementName).item(0);
+    }
+    else {
+      throw new XMLException(new IllegalArgumentException(), "Missing " + elementName + " tag in XML file");
+    }
+  }
+
   // get value of Element's attribute
   protected String getAttribute(String tagName, String attributeName) {
     NodeList nodeList = root.getElementsByTagName(tagName);
@@ -53,14 +63,22 @@ public class XMLGenericParser {
     }
   }
 
+  protected String getAttribute(Element parent, String tagName, String attributeName) throws XMLException {
+    try {
+      return getElement(parent, tagName).getAttribute(attributeName);
+    }
+    catch (IllegalArgumentException e) {
+      throw new XMLException(e, "Missing " + attributeName + " attribute");
+    }
+  }
+
   // get value of Element's text
-  protected String getTextValue(Element e, String tagName) {
+  protected String getTextValue(Element e, String tagName) throws XMLException {
     NodeList nodeList = e.getElementsByTagName(tagName);
     if (nodeList != null && nodeList.getLength() > 0) {
       return nodeList.item(0).getTextContent();
     } else {
-      // FIXME: empty string or exception? In some cases it may be an error to not find any text
-      return "";
+      throw new XMLException(new IllegalArgumentException(), tagName + " tag missing text value");
     }
   }
 
