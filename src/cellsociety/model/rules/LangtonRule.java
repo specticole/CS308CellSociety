fe5ffb61b70fabdd477193b637203001e093c71c
Cole Spector <cgs26@duke.edu>
2021-02-21 16:18:53 -0500

    added Langton State and Rule

---
+++ src/cellsociety/model/rules/LangtonRule.java
@@ -0,0 +1,23 @@
+package cellsociety.model.rules;
+
+import cellsociety.model.Cell;
+import cellsociety.model.CellularAutomatonRule;
+import java.util.List;
+import java.util.Map;
+
+public class LangtonRule extends CellularAutomatonRule {
+
+  /**
+   * Construct a rule with a given parameter map.
+   *
+   * @param params Parameter map from String to String.
+   */
+  public LangtonRule(Map<String, String> params) {
+    super(params);
+  }
+
+  @Override
+  public void advanceCellState(Cell cell, List<Cell> neighbors) {
+
+  }
+}
