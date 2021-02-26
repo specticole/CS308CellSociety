a2d79bc0672ec20662304785130e4007022571ec
Franklin Wei <franklin@rockbox.org>
2021-02-26 16:58:05 -0500

    Finish javadoc for model.

--- src/cellsociety/model/states/LangtonState.java
+++ src/cellsociety/model/states/LangtonState.java
@@ -12,33 +12,54 @@ public class LangtonState extends CellState<LangtonState.States> {
    * Possible states.
    */
   public enum States {
-    //No cell data
+    /**
+     * No cell data
+     */
     OPEN,
 
-    //A trail for the TURN and ADVANCE TO FOLLOW
+    /**
+     * A trail for the TURN and ADVANCE TO FOLLOW
+     */
     PATH,
 
-    //The sheath around a loop
+    /**
+     * The sheath around a loop
+     */
     SHEATH,
 
-    //The trailer behind a moving state
+    /**
+     * The trailer behind a moving state
+     */
     TRAILING,
 
-    //If hits a SHEATH, and not other path is OPEN, extends the PATH by one
+    /**
+     * If hits a SHEATH, and not other path is OPEN, extends the PATH
+     * by one.
+     */
     ADVANCE,
 
-    //If hits a SHEATH, and no other path is OPEN, sets SHEATH to HOLD
-    //If hits a HOLD, and no other path is open, extends 90 deg. clockwise one.
-    //If hits a SHEATH, and two paths are OPEN, sets SHEATH to MESSENGER.
+    /**
+     * If hits a SHEATH, and no other path is OPEN, sets SHEATH to
+     * HOLD If hits a HOLD, and no other path is open, extends 90
+     * deg. clockwise one.  If hits a SHEATH, and two paths are OPEN,
+     * sets SHEATH to MESSENGER.
+     */
     TURN,
 
-    //Tells turn whether or not to create a new turn.
+    /**
+     * Tells turn whether or not to create a new turn.
+     */
     HOLD,
 
-    //Sets branching PATH to SHEATH-OPEN-SHEATH, with OPEN above and below middle OPEN
+    /**
+     * Sets branching PATH to SHEATH-OPEN-SHEATH, with OPEN above and
+     * below middle OPEN
+     */
     MESSENGER,
 
-    //IF loops collide, sets end of loop to killer, killing all TURN and ADVANCE in loop.
+    /**
+     * IF loops collide, sets end of loop to killer, killing all TURN and ADVANCE in loop.
+     */
     KILLER
   }
 
--- src/cellsociety/model/states/RockPaperScissorState.java
+++ src/cellsociety/model/states/RockPaperScissorState.java
@@ -2,23 +2,56 @@ package cellsociety.model.states;
 
 import cellsociety.model.CellState;
 
+/**
+ * A RPS cell state.
+ *
+ * @author Cole Spector
+ */
 public class RockPaperScissorState extends CellState<RockPaperScissorState.States> {
 
+  /**
+   * Allowed state enum.
+   */
   public enum States {
+    /**
+     * Rock
+     */
     ROCK,
+    /**
+     * Paper
+     */
     PAPER,
+    /**
+     * Scissors
+     */
     SCISSOR,
+    /**
+     * Empty cell.
+     */
     EMPTY
   }
 
+  /**
+   * Create a state.
+   *
+   * @param s State enum.
+   */
   public RockPaperScissorState(States s) {
     super(s);
   }
 
+  /**
+   * Create an empty state.
+   */
   public RockPaperScissorState(){
     this(States.EMPTY);
   }
 
+  /**
+   * Create a state.
+   *
+   * @param str State string.
+   */
   public RockPaperScissorState(String str) {
     super(States.class, str);
   }
